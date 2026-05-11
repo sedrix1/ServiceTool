be8d206810dddffb740875dad412db8c56478a6e
Sedrix <francescoguzzini08@gmail.com>
2026-05-11 22:08:19 +0200

    Added more services and bugs fixed
    - Bug Fixed: Start Type Auto
    - Bug Fixed: Start Type Disabled
    - Added 16 Services
    - Improved Interface
    - Some Adjustments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Service Tool
+# Windows Service Tool
+
 Windows batch utility tool for managing and monitoring essential system services, allowing users to enable, disable, and check service status through a simple CLI interface, with basic system optimization and performance tweaks.
--- svctool.bat
+++ svctool.bat
@@ -18,7 +18,7 @@ for /f %%a in ('echo prompt $E ^| cmd') do set "ESC=%%a"
 
 reg add HKCU\Console /v VirtualTerminalLevel /t REG_DWORD /d 1 /f >nul 2>&1
 
-set services=pcasvc DiagTrack SysMain SgrmBroker CDPUserSvc WSearch
+set services=pcasvc DiagTrack DPS SysMain SgrmBroker CDPUserSvc WSearch TabletInputService wisvc RemoteRegistry Fax MapsBroker dmwappushservice WerSvc RetailDemo WpcNetworkSvc TermService lfsvc XboxNetApiSvc XblAuthManager XblGameSave XboxGipSvc 
 
 chcp 65001 > nul
 
@@ -42,6 +42,7 @@ echo.
 goto :eof
 
 :menu
+echo.
 call :banner
 echo.
 echo            [1] Enable Services
@@ -54,33 +55,36 @@ echo.
 set /p input="            [>] Select an option: "
 echo. 
 
-if "%input%"=="1" goto attiva
-if "%input%"=="2" goto disattiva
-if "%input%"=="3" goto visualizza
-if "%input%"=="4" goto esci
+if "%input%"=="1" goto enable
+if "%input%"=="2" goto disable
+if "%input%"=="3" goto view
+if "%input%"=="4" goto fexit
 
 echo Scelta non valida
 pause
 goto menu
 
-:visualizza
+:view
+echo.
 call :banner
 echo.
-echo %ESC%[97m[STATO SERVIZI]%ESC%[0m
-powershell -Command "get-service | findstr -i "pcasvc"; get-service | findstr -i "DPS"; get-service | findstr -i "Dialogtrack"; get-service | findstr -i "sysmain"; get-service | findstr -i "eventlog"; get-service | findstr -i "sgrmbroker"; get-service | findstr -i "cdpusersvc"; get-service | findstr -i "appinfo"; get-service | findstr -i "WSearch" | findstr -i "VSS"; get-service | findstr -i "vss""
+echo %ESC%[97m[SERVICES STATUS]%ESC%[0m
+set "SERVICEVIEWLIST=Appinfo,CDPUserSvc*,DiagTrack,dmwappushservice,DPS,EventLog,Fax,lfsvc,MapsBroker,PcaSvc,RemoteRegistry,RetailDemo,SgrmBroker,SysMain,TabletInputService,TermService,VSS,WerSvc,wisvc,WpcNetworkSvc,WSearch,XblAuthManager,XblGameSave,XboxGipSvc,XboxNetApiSvc"
+powershell -NoProfile -ExecutionPolicy Bypass "$arr=$env:SERVICEVIEWLIST -split ','; $list=Get-Service -Name $arr -ErrorAction SilentlyContinue | Sort-Object Name; foreach($s in $list){ $wmi=Get-CimInstance Win32_Service -Filter \"Name='$($s.Name)'\"; $startType=$wmi.StartMode; if($s.Status -eq 'Stopped' -or $startType -eq 'Disabled'){ Write-Host '[DISABLED] ' -NoNewline -ForegroundColor Red; Write-Host ($s.Name.PadRight(22) + ' - State: ' + $s.Status + ' - Type: ' + $startType) } else { Write-Host '[ENABLED]  ' -NoNewline -ForegroundColor Green; Write-Host ($s.Name.PadRight(22) + ' - State: ' + $s.Status + ' - Type: ' + $startType) } }"
 echo.
 echo.
 echo.
 pause
 goto menu
 
-:attiva
+:enable
+echo.
 call :banner
 echo.
 echo %ESC%[92m[ENABLING SERVICES]%ESC%[0m
 for %%s in (%services%) do (
     echo Starting process %%s...
-    sc config %%s start = auto >nul 2>&1
+    sc config %%s start= auto >nul 2>&1
     powershell -Command "Start-Service %%s -ErrorAction SilentlyContinue"
     if !errorlevel! equ 0 (
         echo %ESC%[92m[OK]%ESC%[0m Successfully enabled process %%s
@@ -94,13 +98,14 @@ echo.
 pause
 goto menu
 
-:disattiva
+:disable
+echo.
 call :banner
 echo.
 echo %ESC%[91m[DISABLING SERVICES]%ESC%[0m
 for %%s in (%services%) do (
     echo Stopping process %%s...
-    sc config %%s start = disabled >nul 2>&1
+    sc config %%s start= disabled >nul 2>&1
     powershell -Command "Stop-Service %%s -ErrorAction SilentlyContinue"
     if !errorlevel! equ 0 (
         echo %ESC%[92m[OK]%ESC%[0m Successfully disabled process %%s
@@ -114,6 +119,6 @@ echo.
 pause
 goto menu
 
-:esci
+:fexit
 echo Exiting...
 exit
